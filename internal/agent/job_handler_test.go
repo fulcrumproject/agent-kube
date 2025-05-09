e6456f582c4a038dfa39884bc77a8c84f8dc23a8
Ernesto Colombo <ernesto.colombo@contactlab.com>
2025-05-09 11:47:50 +0200

    agent wip

--- internal/agent/job_handler_test.go
+++ internal/agent/job_handler_test.go
@@ -13,16 +13,17 @@ func TestJobHandler(t *testing.T) {
 	jobHandler := NewJobHandler(fulcrumCli, proxmoxCli, kamajiCli, sshCli)
 
 	t.Run("Full lifecycle", func(t *testing.T) {
-		// 1. Job to create a cluster service with 1 node (id: node1, size: s1, state: on)
-		// 2. Job to update the cluster service adding a node (id: node2, size: s2, state: on)
-		// 3. Job to update the cluster service making node2 off
-		// 4. Job to update the cluster service making node2 on
-		// 5. Job to update the cluster service making node2 off
-		// 6. Job to stop the cluster service
-		// 7. Job to update the cluster service making node2 on
-		// 8. Job to start the cluster service
-		// 9. Job to stop the cluster service
-		// 10. Job to delete the cluster service
+		// Job to create a cluster service with 1 node (id: node1, size: s1, state: on)
+		// Job to start the cluster service
+		// Job to update the cluster service adding a node (id: node2, size: s2, state: on)
+		// Job to update the cluster service making node2 off
+		// Job to update the cluster service making node2 on
+		// Job to update the cluster service making node2 off
+		// Job to stop the cluster service
+		// Job to update the cluster service making node2 on
+		// Job to start the cluster service
+		// Job to stop the cluster service
+		// Job to delete the cluster service
 	})
 
 }
